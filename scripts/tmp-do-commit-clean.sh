6aff7927d64a39e45956416a2d4d5e1485bf72a2
mhamad4968 <m.hamad4968@gmail.com>
2026-05-07 22:43:36 +0900

    chore(scripts): cleanup 3 tmp scripts erroneously committed in 5fc95ee [skip ci]

--- scripts/tmp-do-commit-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")/.."
-git add -A
-echo STAGED_COUNT $(git diff --cached --name-only | wc -l)
-git commit -F .git/COMMITMSG_Q33C.txt
-git push origin HEAD:main
-git log --oneline -3
